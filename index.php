e16afeda0f49ab1454ad0b5c006de65cf8b3aee2
jmcgamer55555@yahoo.com <jmcgamer55555@yahoo.com>
2015-09-17 00:03:11 +0000

    Starting out

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+     <h1>Hello, world!</h1>
+    <div class = "container">
+      <div class = "row">
+          <div class = "col-md-8 col-xs-1">
+          <a href ="#" class ="btn btn-success"> New Record </a>
+          <a href = "#" class="btn btn-danger"> Delete all 
+          <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+          </a>
+          <div class="bs-example" data-example-id="hoverable-table">
+   
+    <div class="alert alert-success" role="alert">good job</div>
+
+<div class="alert alert-danger" role="alert">Messed up</div>
+ 
+ <div class="progress">
+  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 15%">
+    <span>45% Complete</span>
+          </div>
+          <div class = "col-md-4 col-xs-1">
+             <table class="table table-hover table-border table-hover table-striped">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                      </tr>
+                    </tbody>
+                  </table>
+        
+  </div>
+          </div>
+      </div>
+          
+          
+</div>
+ 
+ 
+  </div>
+
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript">
+    
+  </body>
+</html>
