c67a3862921ff3b3d4866f1371cc6f33e0c4ac04
jmcgamer55555@yahoo.com <jmcgamer55555@yahoo.com>
2015-10-01 14:42:10 +0000

    in class

---
+++ php/food/edit.php
@@ -0,0 +1,131 @@
+<?php
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Food Log</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" type="text/css" />
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  </head>
+  <body>
+    <div class="container">
+          <div class='alert alert-warning'>
+            <button type="button" class="close" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <b>Special Offer</b> Free ice cream today!
+          </div> 
+
+        <div class="page-header">
+          <h1>Runs <small>Record your daily intake</small></h1>
+        </div>
+        <!-------------------------------start of form----------------------------------------------------------------->
+        <form class="form-horizontal" action="./" method="post" >
+          <div class='alert' style="display: none" id="myAlert">
+            <button type="button" class="close" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <h3></h3>
+          </div> 
+          <div class="form-group">
+            <label for="txtName" class="col-sm-2 control-label">Name</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="txtName" name="Name" placeholder="Meal; Name">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="txtCallories">Callories</label>
+            <div class="col-sm-10">
+                  <input type="number" class="form-control" id="txtCallories" name= "Callories" placeholder="Callories">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="txtDate">When</label>
+            <div class="col-sm-10">
+                  <input type="date" class="form-control" id="Time" placeholder="Date">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Remember me
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <button type="submit" class="btn btn-success" id="submit">Record</button>
+            </div>
+          </div>
+        </form>
+        <div class="progress">
+              <div class="progress-bar progress-bar-striped active" role="progressbar"  style="width: 0%">
+                <span >0</span>% Complete
+              </div>
+        </div>
+     
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script type="text/javascript">
+      (function($){
+        $(function(){
+          
+          $("#submit").on('click', function(e){
+            var self = this;
+            //$(self).css({display: "none"});
+            $(self).hide().after("Working...");
+           // $("form").submit();
+            var per = 0;
+            var interval = setInterval(function(){
+              per += 25;
+              $(".progress-bar").css("width", per + "%");
+              $(".progress-bar span").text(per);
+              if(per >= 100){
+                clearInterval(interval);
+                
+                if( !$("#txtDate").val() ){
+                  $("input").css({ backgroundColor: "#FFAAAA"});
+                  $(self).prop("disabled", false).html("Try Again");
+                  $("#myAlert").removeClass("alert-success").addClass("alert-danger").show()
+                    .find("h3").html("You must enter a date");
+                  toastr.error("You must enter a date");
+                  
+                }else{
+                  // Display success
+                  $("#myAlert").removeClass("alert-danger").addClass("alert-success").show()
+                    .find("h3").html("Yay! You did it.");
+                  toastr.success("Yay! You did it.")
+                  
+                }
+                
+                
+              }
+            }, 200);
+            return false;
+          });
+          $(".close").on('click', function(e) {
+              $(this).closest(".alert").slideUp()
+          });
+          $("input[type='number']").spinner();
+          $("input[type='date']").datepicker();
+        });
+      })(jQuery);
+    </script>
+  </body>
+</html>
--- php/food/index.php
+++ php/food/index.php
@@ -1,19 +1,19 @@
-<?php //"php" is not needed for most sever c9 needs this
+<?php
     $name = 'Justin Cody';
-    $message = "Welcome $name"; //single quote is literal and double quotes look for varibles in the string
-    $person = array('Name' => $name,'Age' => 22, CallorieGoal => 2000);
+    $message = "Welcome $name";
+    
+    $person = array( 'Name' => $name, 'Age' => 22, CallorieGoal => 2000 );
+    
     $food = array(
-        array('Name' => 'Breakfast', 'Time' => strtotime("-1 hour"), Callories => 400),
-        array('Name' => 'Lunch', 'Time' => strtotime("now"), Callories => 800),
-        array('Name' => 'Snack', 'Time' => strtotime("now + 1 hour"), Callories => 600),
-        array('Name' => 'Dinner', 'Time' => strtotime("6pm"), Callories => 300),
+        array( 'Name' => 'Breakfast', 'Time' => strtotime("-1 hour"), Callories => 400 ),
+        array( 'Name' => 'Lunch', 'Time' => strtotime("now"), Callories => 800 ),
+        array( 'Name' => 'Snack', 'Time' => strtotime("now + 1 hour"), Callories => 400 ),
+        array( 'Name' => 'Dinner', 'Time' => strtotime("6pm"), Callories => 400 ),
         );
     $total = 0;
-    foreach ($food as $meal) {     //food is the list meal is the interator varible
+    foreach ($food as $meal) {
         $total += $meal['Callories'];
-        
     }
-    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,7 +22,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Food intake</title>
+    <title>Food Intake</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -30,26 +30,25 @@
   <body>
     <div class="container">
             <h1>Food Intake</h1>
-            <h2>Welcome <?=$name?></h2>
-            <div class="panel panel-success">...</div>
-                <div class = "panel-heading"> Your Data</div>
-                <div class = "panel-bod">
-                    <dl class = "dl- horizontal">
+            <h2><?=$message?></h2>
+            <div class="panel panel-success">
+                <div class="panel-heading">Your Data</div>
+                <div class="panel-body">
+                    <dl class="dl-horizontal">
                         <dt>Name</dt>
-                        <dd><?=$person['Name']?> </dd>
+                        <dd><?=$person['Name']?></dd>
                         <dt>Age</dt>
-                        <dd><?=$person['Age']?> </dd>
+                        <dd><?=$person['Age']?></dd>
                         <dt>Goal</dt>
-                        <dd><?=$person['CallorieGoal']?> </dd>
-                        <dt>Today's intake</dt>
-                        <dd><?=$total?> </dd>
+                        <dd><?=$person['CallorieGoal']?></dd>
+                        <dt>Today's Intake</dt>
+                        <dd><?=$total?></dd>
                     </dl>
                 </div>
-
-
+            </div>
       <div class="row">
         <div class="col-md-8 col-xs-10">
-            <a href="#" class="btn btn-success">
+            <a href="edit.php" class="btn btn-success">
                 <i class="glyphicon glyphicon-plus"></i>
                 New Record
             </a>
@@ -61,30 +60,24 @@
             <table class="table table-condensed table-striped table-bordered table-hover">
               <thead>
                 <tr>
-                    <?php $name = $food[0];?>
                   <th>#</th>
-                  <? foreach($names as $n): ?>
-                  <th><?=$n?></th>
-                  <?endforeach; ?>
+                  <th>Name</th>
+                  <th>Time</th>
+                  <th>Callories</th>
                 </tr>
               </thead>
               <tbody>
-                  <?php foreach($food as $i =>  $meal): ?> 
+                <?php foreach($food as $i => $meal): ?>
                 <tr>
-                  <th scope="row"><?=$i+1?></th>
-                  <td><?$meal['Name']?></td>
-                  <td><?=date("M d Y  h:i:sa",$meal['Time'])?></td>
-                  <td><?$meal['Callories']?></td>
+                  <th scope="row"><?=$i?></th>
+                  <td><?=$meal['Name']?></td>
+                  <td><?=date("M d Y  h:i:sa", $meal['Time'])?></td>
+                  <td><?=$meal['Callories']?></td>
                 </tr>
-              <?php endforeach; ?>
+                <?php endforeach; ?>
               </tbody>
             </table>  
           
-        </div>
-        <div class="col-md-4 col-xs-2">
-            <span class="label label-success">Success</span>
-            <span class="label label-info">Info</span>
-            <span class="label label-warning">Warning</span>
         </div>
         <div class="col-md-4 col-xs-10">
             <div class="alert alert-success" role="alert">
@@ -93,12 +86,7 @@
             <div class="alert alert-danger" role="alert">
                 Oh no! You messed up.
             </div>
-            <div class="progress">
-             
 
-              </div>
-            </div>
-          
         </div>
       </div>
       
@@ -107,8 +95,5 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script type="text/javascript">
-;
-    </script>
   </body>
 </html>
