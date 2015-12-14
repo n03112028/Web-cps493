c33e6070cd8cd39ce1c78a53263d4c4561093ff2
Justin Cody <jmcgamer55555@yahoo.com>
2015-12-14 15:11:09 +0000

    update

--- php/Views/shared/_Template.php
+++ php/Views/shared/_Template.php
@@ -7,17 +7,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Food Intake</title>
-
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   </head>
   <body>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+      <style>
+body  {
+    background-image: url("http://www.wallpaper77.com/upload/DesktopWallpapers/cache/Black-Hole-ipad-3-wallpaper-ipad-wallpaper-retina-display-wallpaper-the-new-ipad-wallpaper-2048x2048-wallpapers-background-patterns-pattern-wallpapers-1920x1200.jpg");
+    background-color: #cccccc;
+
+}
+</style>
+    
+      <marquee width="100%" height="150px" direction="up" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()" scrollamount="1" scrolldelay="1" style="height: 150px; width: 100%;">
+      <p style="text-align:center"><b>Welcome to Fitness buddy</b></p>
+      <p style="text-align:center"><b>The underdog fitness app</b></p>
       
+      <p style="text-align:center"></p>   
+
+<!--End of Text Marquee-->
+   </marquee></td>
     <div class="container">
-        <h1>My Website</h1>
               <nav class="navbar navbar-default navbar-inverse">
         <div class="container-fluid">
           <div class="navbar-header">
@@ -27,29 +40,23 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="#">Fitness Buddy</a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
               <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <li><a href="About.html">About</a></li>
+              <li><a href="Contact.html">Contact</a></li>
+              <li class="Dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Info <span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
+                  <li><a href="Fitness/index.php">Fitness</a></li>
+                  <li><a href="Diet/index.php">Diet</a></li>
                 </ul>
               </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+         
             </ul>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
